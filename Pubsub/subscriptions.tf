acb448b80c92f4b25498a4f764c9b5f224601e62
Ramesh S <rameshsudhan11@gmail.com>
2024-10-20 15:03:21 +0530

    Adding subs

---
+++ Pubsub/subscriptions.tf
@@ -0,0 +1,36 @@
+//SUBSCRIPTION 1 (PULL)
+resource "google_pubsub_subscription" "subscription_1" {
+  name  = "test.subscription.1"
+  topic = "test.topic.1"
+
+  message_retention_duration = "86400s"
+  retain_acked_messages      = false
+
+  ack_deadline_seconds = 10
+
+}
+
+//SUBSCRIPTION 2 (PULL)
+resource "google_pubsub_subscription" "subscription_2" {
+  name  = "test.subscription.2"
+  topic = "test.topic.1"
+
+  message_retention_duration = "86400s"
+  retain_acked_messages      = false
+
+  ack_deadline_seconds = 10
+
+}
+
+//SUBSCRIPTION 3 (PUSH)
+
+resource "google_pubsub_subscription" "subscription_3" {
+  name  = "test.subscription.push.1"
+  topic = "test.topic.1"
+
+  ack_deadline_seconds = 10
+  message_retention_duration = "604800s"
+  push_config {
+    push_endpoint = "https://webhook.site/5611850b-cd7c-4681-afab-c39a117d1515"
+  }
+}
